fb4300f2b8b384eb4250b7934e9c9e3d6250e7d7
MachaSreenath <sreenathshree9@gmail.com>
2024-11-23 17:07:39 +0530

    roboshop shell scripts

--- catalogue.sh
+++ catalogue.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-mongodb_host=mongodb.forpractice.online
+mongodb_host=mongodb.forpractice.uno
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
