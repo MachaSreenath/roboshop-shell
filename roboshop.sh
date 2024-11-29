541610b680ea1893b5f661ed9a5ff1fb232f6ed7
MachaSreenath <sreenathshree9@gmail.com>
2024-11-29 20:00:11 +0530

    roboshop script changes

--- roboshop.sh
+++ roboshop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-AMI=ami-0f3c7d07486cad139 #this keeps on changing
-SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
+AMI=ami-0b4f379183e5706b9 #this keeps on changing
+SG_ID=sg-0b5846436e6cc4f05 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
-DOMAIN_NAME="daws76s.online"
+ZONE_ID=Z016460536HW3XT7VUB95 # replace your zone ID
+DOMAIN_NAME="forpractice.uno"
 
 for i in "${INSTANCES[@]}"
 do
